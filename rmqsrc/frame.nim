f41c2560b9c53e1efaa5c91bd328feca0a26b9fa
Zach Smith <zd@zdsmith.com>
2018-01-03 12:01:26 -0500

    Read Params from String Stream

--- rmqsrc/frame.nim
+++ rmqsrc/frame.nim
@@ -1,4 +1,4 @@
-import strutils, options
+import strutils, options, streams, endians
 
 const
   VERSION_MAJOR = 0.char
@@ -13,6 +13,10 @@ type
     of fkProtocol:
       major, minor, revision: cchar
   DecodedFrame* = tuple[consumed: int, frame: Option[Frame]]
+  FrameParams = tuple
+    frameType: uint8
+    frameChannel: uint16
+    frameSize: uint32
 
 proc `$`*(frame: Frame): string =
   "Frame: $#" % $frame.kind
@@ -33,6 +37,24 @@ proc protocolHeader*: Frame = initProtocolHeader(
   VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION
 )
 
+proc readFrameType(s: Stream): uint8 =
+  result = s.readUInt8
+
+proc readChannelNumber(s: Stream): uint16 =
+  result = s.readUInt16
+  bigEndian16(addr result, addr result)
+
+proc readFrameSize(s: Stream): uint32 =
+  result = s.readUInt32
+  bigEndian32(addr result, addr result)
+
+proc readFrameParams(s: Stream): FrameParams =
+  let
+    frameType = s.readFrameType
+    channelNumber = s.readChannelNumber
+    frameSize = s.readFrameSize
+  (frameType, channelNumber, frameSize)
+
 proc decode*(data: string): DecodedFrame =
   if data.startsWith("AMQP"):
     try:
@@ -45,5 +67,7 @@ proc decode*(data: string): DecodedFrame =
     except IndexError:
       result = (0, none Frame)
   else:
-    # TODO: Implement rest if frame types.
+    var stringStream = newStringStream(data)
+    let frameParams = readFrameParams(stringStream)
+    echo "Frame Params", frameParams
     result = (0, none Frame)
