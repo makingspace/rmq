bc44c94e3716b87ca0a285f40bea5c9c929085ac
Maxim Pertsov <maxim.pertsov@gmail.com>
2018-01-04 22:05:22 -0500

    Encode value types

--- rmqsrc/encode.nim
+++ rmqsrc/encode.nim
@@ -1,5 +1,5 @@
 import endians, tables, strutils, sequtils
-import spec, frame, methods
+import spec, frame, methods, values
 
 # Encode basic types
 proc encode(v: uint8): array[0..0, char] =
@@ -16,49 +16,55 @@ proc encode(v: uint32): array[0..3, char] =
 proc encode(v: string): seq[char] =
   v.mapIt(it.char)
 
-proc encode(v: Table[string, string]): seq[char] =
-  # TODO fill this out, right now we will just assume the table is empty
+proc encode(vnode: ValueNode): seq[char] =
   result = newSeq[char]()
-  for ch in encode(0.uint32):
-    result.add(ch)
+  case vnode.valueType
+  of vtShortStr:
+    result.add(encode(vnode.shortStrValue.len.uint8))
+    result.add(encode(vnode.shortStrValue))
+  of vtLongStr:
+    result.add(encode(vnode.longStrValue.len.uint32))
+    result.add(encode(vnode.longStrValue))
+  of vtTable:
+    # FIXME: this only works for empty tables
+    result.add(encode(0.uint32))
+  else:
+    raise newException(ValueError, "Encode")
 
 # Define encodings method parameters
 type 
-  ConnectionStartOk = tuple
-    index: uint32
-    clientProps: Table[string, string]
-    mechanism: (string, uint8) # short string
-    response: (string, uint32) # long string
-    locale: (string, uint8)    # short string
+  MethodParams = seq[tuple[name: string, valueType: ValueType]]
 
-proc encode*(v: ConnectionStartOk): seq[char] =
+const
+  paramsLookup: Table[MethodId, MethodParams] = {
+    mStartOk: @[("clientProps", vtTable), ("mechanism", vtShortStr), ("response", vtLongStr), ("locale", vtShortStr)]
+  }.toTable
+
+proc encode*(mid: MethodId, params: varargs[ValueNode]): seq[char] =
+  let reqParams = paramsLookup[mid]
+  if reqParams.len != params.len:
+      raise newException(ValueError, "$# arguments required, but only supplied $#" % [$reqParams.len, $params.len])
+
   result = newSeq[char]()
+  for i in 0 .. reqParams.high:
+    if reqParams[i].valueType != params[i].valueType:
+      raise newException(ValueError, "Argument $# ($#) must have value type $#" % [$i, $reqParams[i].name, $params[i].valueType])
 
-  result.add(v.index.encode())
-  result.add(v.clientProps.encode())  # FIXME: this only works for empty tables at the moment
-  result.add(v.mechanism[1].encode())
-  result.add(v.mechanism[0].encode())
-  result.add(v.response[1].encode())
-  result.add(v.response[0].encode())
-  result.add(v.locale[1].encode())
-  result.add(v.locale[0].encode())
-
-proc newConnectionStartOk*(clientProps: Table[string, string], mechanism: string = "PLAIN", response: string, locale: string = "en_US"): seq[char] =
-  let m = (index: 0x000A000B.uint32,   # TODO can we specify functions by IDs instead of hardcoding this?
-           clientProps: clientProps,
-           mechanism: (mechanism, len(mechanism).uint8),
-           response: (response, len(response).uint32),
-           locale: (locale, len(locale).uint8))
-  return m.encode()
+    result.add(params[i].encode())
 
 # Encode frame components
-proc encode*(rpcMethod: Method): seq[char] =
+proc encode*(m: Method): seq[char] =
   result = newSeq[char]()
-  case rpcMethod.kind
+  case m.kind
   of mStartOk:
-    result.add(newConnectionStartOk(rpcMethod.serverPropertiesOk, rpcMethod.mechanismsOk, rpcMethod.responseOk, rpcMethod.localesOk))
+    # FIXME only works if table is empty
+    result.add(encode(m.kind,
+                      ValueNode(valueType: vtTable),
+                      ValueNode(valueType: vtShortStr, shortStrValue: m.mechanismsOk),
+                      ValueNode(valueType: vtLongStr, longStrValue: m.responseOk),
+                      ValueNode(valueType: vtShortStr, shortStrValue: m.localesOk)))
   else:
-    raise newException(ValueError, "Cannot encode: undefined method of '$#'" % [$rpcMethod.kind])
+    raise newException(ValueError, "Cannot encode: undefined method of '$#'" % [$m.kind])
 
 proc encode*(frame: Frame): seq[char] =
   result = newSeq[char]()
@@ -85,7 +91,7 @@ when isMainModule:
   echo encode(uint16(1))
   echo encode(uint32(1))
   echo encode("hi")
-  echo newConnectionStartOk(initTable[string, string](), "PLAIN", "hi", "en_US")
+  # echo newConnectionStartOk(initTable[string, string](), "PLAIN", "hi", "en_US")
 
   var rpcMethod = Method()
 
